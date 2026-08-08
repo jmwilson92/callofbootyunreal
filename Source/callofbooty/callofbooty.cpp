64c62357bf7f4f58d6b4fb2f07517d7c7d3c5457
Jeramey Wilson <jerame.wilson@gmail.com>
2026-08-07 20:18:00 -0700

    Initial UE5 project

---
+++ Source/callofbooty/callofbooty.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "callofbooty.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, callofbooty, "callofbooty" );
+
+DEFINE_LOG_CATEGORY(Logcallofbooty)
---
+++ Source/callofbooty/callofbootyGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "callofbootyGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class AcallofbootyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AcallofbootyGameMode();
+};
+
+
+
